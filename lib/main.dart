f6b9225140597ccd48c49ebe5956edba19415d73
mizuhomai <choco.hana.choco@gmail.com>
2021-10-05 16:38:23 +0900

    イメージ設置前

--- lib/main.dart
+++ lib/main.dart
@@ -16,9 +16,41 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           leading: Icon(Icons.video_camera_back),
           title: const Text('Melon Groove'),
+          actions: <Widget>[
+            SizedBox(
+              width: 30,
+              child: TextButton(
+                  onPressed: onPressed(),
+                  child: Icon(
+                    Icons.search,
+                  )),
+            ),
+            TextButton(onPressed: onPressed(),
+                child: Icon(
+                    Icons.more_vert,
+                )),
+          ],
         ),
-        body: Container(),
+        body: SizedBox(
+          width: double.infinity,
+          child: Container(
+            decoration: BoxDecoration(color: Colors.pink),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    SizedBox(
+                        width:300,child: Image.network('https://cdn.icon-icons.com/icons2/2107/PNG/512/file_type_flutter_icon_130599.png')),
+                  ],
+                ),
+              ],
+
+            ),
+          ),
+        ),
       ),
     );
   }
+
+  onPressed() {}
 }
