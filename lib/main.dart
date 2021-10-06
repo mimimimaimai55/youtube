c7378f0511f1a15baa0f0fa8cd89dc4142b989c7
mizuhomai <choco.hana.choco@gmail.com>
2021-10-06 18:55:20 +0900

    youtubeアプリ 1ページ目の見た目がほぼ完成

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'video_detail_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
+  final items = List<String>.generate(10000, (i) => "Item $i");
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,7 +18,10 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       home: Scaffold(
         appBar: AppBar(
-          leading: Icon(Icons.video_camera_back),
+          leading: Icon(
+            Icons.video_camera_back,
+          color:Colors.red,
+          ),
           title: const Text('Melon Groove'),
           actions: <Widget>[
             SizedBox(
@@ -32,25 +39,96 @@ class MyApp extends StatelessWidget {
           ],
         ),
         body: Container(
-          //横方向の真ん中合わせ
-            child: Center(
-              child: Column(
-                //縦方向に並べるカラムを中央に配置
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Row(
-                    //よくわかってない
-                    mainAxisSize: MainAxisSize.min,
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+
+                  child: Row(
                     children: <Widget>[
-                      Container(
-                          child:Image.network(
-                              'https://hiyokoyarou.com/wp-content/uploads/2014/08/catsn.png'
+
+                      SizedBox(
+                        // width: 40,
+                        // height:40,
+                        child: Image.network(
+                            'https://hiyokoyarou.com/wp-content/uploads/2014/08/catsn.png',
+                            width: 70, height:70,
+                        ),
+                      ),
+                      Column(
+                        children: <Widget>[
+                          const Text('Melon Groove',
+                            style: TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
-                          width: 300, height:300, ),
+                          TextButton(onPressed: onPressed(),
+                              child: Row(
+                                children:<Widget> [
+                                  Text(
+                                    'チャンネル登録',
+                                style: TextStyle(
+                                  color: Colors.red[800],
+                                  fontSize: 18,
+                                ),
+                                  ),
+                                ],
+                              )),
+                        ],
+
+                      ),
                     ],
-                  )
-                ],
-              ),
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: double.infinity,
+                    child: Text('人気のアップロード動画',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: items.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        onTap: ()async{
+                          //TODO:画面遷移する
+                          await Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => VideoDetailPage()),
+                          );
+                        },
+                        contentPadding: EdgeInsets.all(8),
+                        leading: Image.network(
+                          'https://i.ytimg.com/vi/y0Blq-W0MZM/maxresdefault.jpg',
+                        ),
+                        title: Column(
+                          children: [
+                            Text('Japanese City Pop Mixtape Vol.20 - Summer Night［70-80s］'),
+                            Row(
+                              children: <Widget>[
+                                Text('23万回視聴・5ヶ月前'),
+                              ],
+                            ),
+
+                          ],
+                        ),
+                          //末尾に表示するWidget ↓
+                          trailing: Icon(Icons.more_vert),
+                      );
+                    },
+                  ),
+                )
+
+              ],
             ),
         ),
       ),
